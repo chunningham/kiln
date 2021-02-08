0cdefe53044c969320baf80ba9856002d4b891a5
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-02-08 18:15:15 +0100

    feat(node): initial impl of rpc node struct

--- src/tezos/node.rs
+++ src/tezos/node.rs
@@ -1,1 +1,18 @@
+use async_trait::async_trait;
+use tezos_api::apis::{configuration::Configuration, default_api::context_big_maps_big_map_id_script_expr_get};
+use anyhow::Result;
 
+pub struct TezosNode(Configuration);
+
+#[async_trait]
+impl TezosNode {
+    pub fn new(url: &str) -> Self {
+        let mut cfg = Configuration::new();
+        cfg.base_path = url.to_owned();
+        cfg
+    }
+
+    async fn get_big_map_entry(big_map_id: &str, key_scriptexpr: &str) -> Result<String> {
+        contect_big_maps_big_map_id_script_expr_get(&self.0, big_map_id, key_scriptexpr).await
+    }
+}
